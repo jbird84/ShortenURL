02e1e5a8d265052ce18ee7716eacf890ee144b67
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-12-04 15:59:09 -0600

    Initial Commit

---
+++ ShortenURL/ShortenURLApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortenURLApp.swift
+//  ShortenURL
+//
+//  Created by Kinney Kare on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct ShortenURLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
